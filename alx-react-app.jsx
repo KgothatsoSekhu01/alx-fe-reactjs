9e30151959a44c07a76a666c57244c9d35a15805
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-06 20:47:49 +0200

    Add files via upload

---
+++ alx-react-app.jsx
@@ -0,0 +1,9 @@
+cd alx-fe-reactjs
+npm create vite@latest alx-react-app -- --template react
+cd alx-react-app
+npm install
+npm run dev
+git add .
+git commit -m "Initial commit with React app setup"
+git push origin main
+
