19f2d224addfccfaa7f02c1a26b7722c7afb653d
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-10 00:11:41 +0200

    Delete alx-react-app.jsx

--- alx-react-app.jsx
+++
@@ -1,9 +0,0 @@
-cd alx-fe-reactjs
-npm create vite@latest alx-react-app -- --template react
-cd alx-react-app
-npm install
-npm run dev
-git add .
-git commit -m "Initial commit with React app setup"
-git push origin main
-
