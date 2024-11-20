f4e99ab3e7d4cd86d012ce0ea83ae9faa97c4ef4
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-20 16:35:18 +0200

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -24,5 +24,18 @@ function App() {
     </Router>
   );
 }
+import React from 'react';
+import SearchBar from './components/SearchBar';
+import RecipeList from './components/RecipeList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <SearchBar />
+      <RecipeList />
+    </div>
+  );
+};
 
 export default App;
