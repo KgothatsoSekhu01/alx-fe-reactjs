18ce55cabafd595aaeb0f045182313a16ad8e497
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-20 10:28:56 +0200

    Delete alx-react-app-new/src/components/UserContext.js

--- alx-react-app-new/src/components/UserContext.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// Create a context with a default value (optional, but helpful for fallback)
-const UserContext = React.createContext();
-
-export default UserContext;
