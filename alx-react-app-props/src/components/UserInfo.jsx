a0fa33ef17967f9b548f7a4a38bae7b7cf4042eb
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-20 10:54:40 +0200

    Update UserInfo.jsx

--- alx-react-app-props/src/components/UserInfo.jsx
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import UserDetails from './UserDetails';
+import React, { useContext } from 'react';  // Import useContext hook
+import UserContext from '../UserContext';   // Import UserContext
 
-function UserInfo() {
-  return <UserDetails />;
+function UserProfile() {
+  // Consume user data using useContext
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>          {/* Display the user's name */}
+      <p>Email: {userData.email}</p>        {/* Display the user's email */}
+    </div>
+  );
 }
 
-export default UserInfo;
+export default UserProfile;
