2a19583fbc6236e2dbfd0629c4722602751d43b7
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-20 10:44:39 +0200

    Add files via upload

---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// Create a context with a default value (optional, but helpful for fallback)
+const UserContext = React.createContext();
+
+export default UserContext;
