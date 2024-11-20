d4da1becd3eef4cd24c22d70aef1f5861b03a10f
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-20 16:04:21 +0200

    Update DeleteRecipeButton.jsx

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,4 +1,5 @@
-import { useRecipeStore } from '../recipeStore';
+import useNavigate from 'react-router-dom';  // Import useNavigate
+import useRecipeStore from '../recipeStore';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
