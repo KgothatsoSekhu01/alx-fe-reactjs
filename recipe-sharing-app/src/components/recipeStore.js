b507af980ce33762a3d738fb0383a193cc9b0f87
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-20 15:43:45 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -11,10 +11,12 @@ import create from 'zustand';
 const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  deleteRecipe: (id) => set((state) => ({
+  deleteRecipe: (id) => 
+    set((state) => ({
     recipes: state.recipes.filter((recipe) => recipe.id !== id)
   })),
-  updateRecipe: (id, updatedRecipe) => set((state) => ({
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
     recipes: state.recipes.map((recipe) =>
       recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
     )
