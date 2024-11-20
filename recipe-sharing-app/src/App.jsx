ce7755a8267d7bd289851989acf7e2810d4298ae
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-20 13:58:16 +0200

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -75,6 +75,24 @@ function App() {
   );
 }
 
+// src/App.jsx
+import React from 'react';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm /> {/* Form to add a recipe */}
+      <RecipeList /> {/* Display the list of recipes */}
+    </div>
+  );
+}
+
+export default App;
+
+
 export default App;
 
 import React from "react";
