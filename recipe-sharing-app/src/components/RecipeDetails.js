d3e0016744c824d25fc9ce742c9820046d78a1e9
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-20 15:47:54 +0200

    Update RecipeDetails.js

--- recipe-sharing-app/src/components/RecipeDetails.js
+++ recipe-sharing-app/src/components/RecipeDetails.js
@@ -1,22 +1,24 @@
-import { useParams } from 'react-router-dom';
-import { useRecipeStore } from '../recipeStore';
-import { EditRecipeForm } from './EditRecipeForm';
-import { DeleteRecipeButton } from './DeleteRecipeButton';
+import { useParams from 'react-router-dom';
+import useRecipeStore from '../recipeStore';
+import EditRecipeForm from './EditRecipeForm';
+import DeleteRecipeButton from './DeleteRecipeButton';
 
 const RecipeDetails = () => {
-  const { id } = useParams(); // Get the ID from the URL
+  const { id } = useParams(); // Get the recipe ID from the URL
   const recipe = useRecipeStore((state) =>
     state.recipes.find((recipe) => recipe.id === parseInt(id))
   );
 
-  if (!recipe) return <p>Recipe not found!</p>;
+  if (!recipe) {
+    return <p>Recipe not found!</p>;
+  }
 
   return (
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
-      <EditRecipeForm recipe={recipe} />
-      <DeleteRecipeButton recipeId={recipe.id} />
+      <EditRecipeForm recipe={recipe} />  {/* Pass recipe to the Edit form */}
+      <DeleteRecipeButton recipeId={recipe.id} />  {/* Pass recipe ID to delete */}
     </div>
   );
 };
