051db5bc839e333d8124cf91825a2c8a7e9e0b17
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-20 15:22:09 +0200

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,24 +1,26 @@
 // src/components/RecipeList.jsx
+import { Link } from 'react-router-dom';
 import { useRecipeStore } from '../recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes); // Access the list of recipes from the store
+  const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
-      <h2>Recipe List</h2>
       {recipes.length > 0 ? (
         recipes.map((recipe) => (
-          <div key={recipe.id} style={{ marginBottom: '20px' }}>
-            <h3>{recipe.title}</h3>
+          <div key={recipe.id}>
+            <Link to={`/recipes/${recipe.id}`}>
+              <h3>{recipe.title}</h3>
+            </Link>
             <p>{recipe.description}</p>
           </div>
         ))
       ) : (
-        <p>No recipes added yet!</p>
+        <p>No recipes available!</p>
       )}
     </div>
   );
 };
 
-export default RecipeList;
+export { RecipeList };
