099025990c2943a54c82c9f642c81e201694f598
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-21 11:05:55 +0200

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,37 +1,11 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Profile from './components/Profile';
-import UserProfile from './components/UserProfile';
-import ProtectedRoute from './components/ProtectedRoute';
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 
-function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+const ProtectedRoute = ({ element, isAuthenticated }) => {
+  if (!isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+  return element;
+};
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        
-        {/* Protected Route for Profile */}
-        <Route 
-          path="/profile/*" 
-          element={<ProtectedRoute isAuthenticated={isAuthenticated} element={<Profile />} />} 
-        />
-        
-        {/* Dynamic Route */}
-        <Route path="/user/:userId" element={<UserProfile />} />
-        
-        <Route path="*" element={<h1>Page Not Found</h1>} />
-      </Routes>
-
-      <button onClick={() => setIsAuthenticated(!isAuthenticated)}>
-        Toggle Authentication
-      </button>
-    </Router>
-  );
-}
-
-export default App;
+export default ProtectedRoute;
