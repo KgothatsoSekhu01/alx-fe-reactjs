03cd395f3165242278c2b455e38fa8de11d65287
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-25 14:02:06 +0200

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -31,5 +31,12 @@ function App() {
     </>
   )
 }
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-3xl font-bold">Hello, Tailwind!</h1>
+    </div>
+  );
+}
 
-export default App
+export default App;
