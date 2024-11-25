69081d7567a371944be9d811f3cd6853c4406c2d
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-25 14:31:31 +0200

    Update tailwind.config.js

--- tailwind-react-integration/src/tailwind.config.js
+++ tailwind-react-integration/src/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: [
+  purge: [
     './src/**/*.{js,jsx,ts,tsx}', // Scans all JS/JSX/TS/TSX files in src
     './public/index.html',        // Scans your HTML files
   ],
