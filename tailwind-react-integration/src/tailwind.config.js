4afc58b183a0d334d2244362683a34e9d4fdf94a
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-25 14:21:43 +0200

    Update tailwind.config.js

--- tailwind-react-integration/src/tailwind.config.js
+++ tailwind-react-integration/src/tailwind.config.js
@@ -3,9 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"
   ],
-  "purge", "darkMode", "variants": 'class', // Optional: If you want to enable dark mode using a class
-  theme: {
-    extend: {},
+
+  module.export = {
+  darkMode: 'class', // Optional: If you want to enable dark mode using a class
+ module.export = {
+    theme: {
+    extend: {
+      backgroundColor: ['active'], // Example of extending background color variants
+      },
   },
   plugins: [],
 };
