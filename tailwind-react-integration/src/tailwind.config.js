128932f2067197b8c773f6eb8aaa9be0bf50e65c
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-25 13:57:12 +0200

    Add files via upload

---
+++ tailwind-react-integration/src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: false, // You can customize this if you want dark mode support
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
