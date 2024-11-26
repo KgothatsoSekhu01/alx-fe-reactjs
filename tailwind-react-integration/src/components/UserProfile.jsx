dd14874e5a1307ee4dd94563fb2c6508e1b03912
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-26 09:07:13 +0200

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
         alt="User" 
         className="rounded-full sm:w-24 sm:h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-autor transition-transform duration-300 ease-in-out hover:scale-110"
       />
-      <h1 className="text-lg sm:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">John Doe</h1>
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">John Doe</h1>
       <p className="text-sm sm:text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
