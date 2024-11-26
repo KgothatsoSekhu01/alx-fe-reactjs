afa09bd284c2881dac880e1263401b7b654cf922
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-11-26 08:13:21 +0200

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}', // Path to all source files
+    './public/index.html',        // Path to your HTML files
+  ],
+  darkMode: 'class',  // Enable dark mode via class
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#1E40AF',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['active'], // Enable 'active' variant for backgroundColor
+    },
   },
   plugins: [],
 }
-
