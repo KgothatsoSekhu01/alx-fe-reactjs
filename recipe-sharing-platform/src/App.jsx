1acd8f9f220c0096be0a71543d9a836b26b2ac0e
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-12-02 09:08:03 +0200

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -47,5 +47,19 @@ function App() {
     </div>
   );
 }
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
