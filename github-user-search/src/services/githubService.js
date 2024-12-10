1bc5819bb85eecdcc959c72717f79b6ad6678d56
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-12-10 09:44:45 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -5,3 +5,10 @@ export const fetchUserData = async (username) => {
   const response = await axios.get(url);
   return response.data;
 };
+import axios from 'axios';
+
+export const fetchUserData = async (query) => {
+  const url = `https://api.github.com/search/users?q=${query}`;
+  const response = await axios.get(url);
+  return response.data.items; // Return list of users from the response
+};
