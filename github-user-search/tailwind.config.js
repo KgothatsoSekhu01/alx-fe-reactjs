034af94cc6c265b0fa0cba88f98d31bd8ffcd992
KgothatsoSekhu01 <kgothatsokgabosekhu@gmail.com>
2024-12-10 09:37:03 +0200

    Update tailwind.config.js

--- github-user-search/tailwind.config.js
+++ github-user-search/tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
